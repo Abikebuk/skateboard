55e459a80156eb5c6f446d9c678ec412bf8a45b1
Rettana Muon <rettana.muon@gmail.com>
2022-10-08 23:14:58 +0200

    SKAT-23 update 3D

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,41 +1,76 @@
 import './Home.sass';
-import React, {Canvas, useFrame} from '@react-three/fiber';
-import {useRef, useState} from 'react';
-import SkateboardFull from '../assets/skateboard-full.glb';
-import {useScroll} from '@react-three/drei';
+import {Canvas, useThree} from '@react-three/fiber';
+import {OrbitControls, useGLTF} from '@react-three/drei';
+import skateboardGltf from '../assets/skateboard-full.glb';
 
-
+// eslint-disable-next-line react/display-name, react/prop-types
 function Skateboard(props) {
-  const scroll = useScroll();
+  const {nodes} = useGLTF(skateboardGltf);
+  const {width} = useThree((state) => state.viewport);
+  console.log(nodes);
+  return (
+    <group {...props} dispose={null} scale={width}>
+      {Object.keys(nodes).map((k) => loadSkateboardPart(nodes[k]))}
+    </group>
+  );
+}
+function loadAll(node) {
+  if (node.type === 'Object3D' && node.visible === true) {
+    return loadGroup(node);
+  }
+  if (node.type ==='Group') {
+    return loadGroup(node);
+  }
+  if (node.type === 'Mesh' && node.visible === true) {
+    return loadMesh(node);
+  }
 }
 
-function Box(props) {
-  const ref = useRef();
-  const [width, height] = useThree((state) => state.viewport);
-
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
-  return <mesh
-    {...props}
-    ref={ref}
-    scale={clicked? 1.5 : 1}
-    onClick={() => click(!clicked)}
-    onPointerOver={() => hover(true)}
-    onPointerOut={() => hover(false)}>
-    <boxGeometry args={[1, 1, 1]}/>
-    <meshStandardMaterial color={hovered? 'hotpink' : 'orange'}/>
-  </mesh>
-  ;
+function loadMesh(node) {
+  return (
+    <mesh
+      key={node.name}
+      geometry={node.geometry}
+      position={node.position}
+      scale={node.scale}
+      rotation={node.rotation}
+      material={node.material}>
+      {node.children.map((children) => loadAll(children))}
+    </mesh>
+  );
 }
+
+function loadGroup(node) {
+  return (
+    <group
+      key={node.name}
+      position={node.position}
+      rotation={node.rotation}
+      scale={node.scale}
+
+    >
+      {node.children.map((children) => loadAll(children))}
+    </group>
+  );
+}
+function loadSkateboardPart(node) {
+  switch (node.name) {
+    case 'Skateboard_Sub_assembly1':
+      return (loadAll(node.parent));
+    case 'Skateboard_Sub_assembly2':
+      break;
+    case 'Solid1':
+      break;
+    default:
+      break;
+  }
+}
+
 export default function Home() {
   return <Canvas>
-    <ambientLight />
-    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1}/>
-    <pointLight position={[-10, -10, -10]}/>
-    <Box position={[-1.2, 0, 0]}/>
-    <Box position={[1.2, 0, 0]}/>
-    <Skateboard position={[0, -1, 0]}/>;
+    <ambientLight/>
+    <Skateboard />
+    <OrbitControls/>
   </Canvas>;
 }
+
