06b425ea5dc9e6140e1c356c24bd10bef44e366e
Ramos <ramos_axel@outlook.fr>
2023-01-04 17:01:28 +0100

    Add inscription functionalities

--- src/include/Inscription.js
+++ src/include/Inscription.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import Sidebar from './Sidebar'
 import Header from './Header'
 import '../include/Inscription.scss'
+
+import axios from 'axios';
 
 function Inscription() {    
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confPassword, setConfPassword] = useState('');
-    const [verifPassword, setVerifPassword] = useState('');
+    const navigate = useNavigate();
 
     // function to update state of name with
     // value enter by user in form
@@ -34,40 +37,76 @@ function Inscription() {
             alert("Les password sont différents")
         }
         else {
-            alert("Inscription réussit")
+            axios.post(process.env.REACT_APP_BACK_URL + "/api/users",
+            {
+                username: name,
+                email: email,
+                password: password,
+                dtInscription: Date.now()
+            }).then((res) => {
+                console.log("TEST")
+                navigate("/")
+            }).catch((error)=>{
+                const resp = error.response.data.error
+                console.log(resp)
+                switch (resp.status){
+                    case 400:
+                        switch (resp.message) {
+                            case "password must be at least 6 characters":
+                                alert("Le mot de passe doit contenir plus de 6 caractères")
+                                break;
+                            case "Email already taken":
+                                alert("Email déjà existant")
+                                break;
+                            case "This attribute must be unique":
+                                alert("Username déjà existant")
+                                break;
+                            default:
+                                alert("400")
+                            break;
+                        }
+                        break
+                    default:
+                        alert("Une erreur s'est produite")
+                    break;
+                }
+            })
         }
         e.preventDefault();
     }
+    useEffect(() => {
+        console.log()
+    })
 
     return (
-                        <div className={'container-fluid m-0'}>
-                            <div className={'row'}>
-                                <div className="Axel">
-                                    <header className="App-header">
-                                        <form onSubmit={(e) => { handleSubmit(e) }}>
-                                            <h2> INSCRIPTION</h2>
-                                            <div className='form-group'>
-                                                <label> Username: </label>
-                                                <input type="text" className="form-control" value={name} required onChange={(e) => { handleChange(e) }} /><br />
-                                            </div>
-                                            <div className='form-group'>
-                                                <label>Email: </label>
-                                                <input type="email" className="form-control" value={email} required onChange={(e) => { handleEmailChange(e) }} /><br />
-                                            </div>
-                                            <div className='form-group'>
-                                                <label>Password:</label>
-                                                <input type="password" className="form-control" value={password} required onChange={(e) => { handlePasswordChange(e) }} /><br />
-                                            </div>
-                                            <div className='form-group'>
-                                                <label>Confirm Password:</label>
-                                                <input type="password" className="form-control" value={confPassword} required onChange={(e) => { handleConfPasswordChange(e) }} /><br />
-                                            </div>                                    
-                                            <input type="submit" class="boutonVal" value="VALIDER" />
-                                        </form>
-                                    </header>
-                                </div>
+        <div className={'container-fluid m-0'}>
+            <div className={'row'}>
+                <div className="Axel">
+                    <header className="App-header">
+                        <form onSubmit={(e) => { handleSubmit(e) }}>
+                            <h2> INSCRIPTION</h2>
+                            <div className='form-group'>
+                                <label> Username: </label>
+                                <input type="text" className="form-control" value={name} required onChange={(e) => { handleChange(e) }} /><br />
                             </div>
-                        </div>
+                            <div className='form-group'>
+                                <label>Email: </label>
+                                <input type="email" className="form-control" value={email} required onChange={(e) => { handleEmailChange(e) }} /><br />
+                            </div>
+                            <div className='form-group'>
+                                <label>Password:</label>
+                                <input type="password" className="form-control" value={password} required onChange={(e) => { handlePasswordChange(e) }} /><br />
+                            </div>
+                            <div className='form-group'>
+                                <label>Confirm Password:</label>
+                                <input type="password" className="form-control" value={confPassword} required onChange={(e) => { handleConfPasswordChange(e) }} /><br />
+                            </div>
+                            <input type="submit" class="boutonVal" value="VALIDER" />
+                        </form>
+                    </header>
+                </div>
+            </div>
+        </div>
     );
 }
 
